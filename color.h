8f49adb5dfe190aa66ad2a3b635a48ad831ee657
ZaiqiangWu <wuzaiqiang@zju.edu.cn>
2019-02-07 16:08:40 +0800

    add

---
+++ color.h
@@ -0,0 +1,10 @@
+//
+// Created by wuzaiqiang on 19-2-7.
+//
+
+#ifndef DISPLAYIMAGE_COLOR_H
+#define DISPLAYIMAGE_COLOR_H
+
+#include "raytracing.h"
+vector3<float> red,green,blue;
+#endif //DISPLAYIMAGE_COLOR_H
